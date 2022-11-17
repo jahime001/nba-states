66c5f4f6eb4a38b5fac8124720b5a9ca1aea3f5e
jahime001 <jahimecamea@gmail.com>
2022-11-17 11:32:08 -0500

    removed video

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import videobg from "../../assets/nbamix.mp4";
 import './Home.css'
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
         <div className="home">
             <div className="video">
                 <div className="overlay"></div>
-                {/* <video src={videobg} autoPlay loop /> */}
                 <h1>Nba Stats for the 2022 Nba Season</h1>
                 <Link to='teams' className="team-check"><h1>Check Out Teams</h1></Link>
 
